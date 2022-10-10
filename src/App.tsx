c4a15e506abe362188fded1e39848b82525dc273
Krzysztof Z <45123514+Zlvsky@users.noreply.github.com>
2022-10-10 14:54:33 +0200

    Created blank react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import  Hero from "./Sections/Hero";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero />
     </div>
   );
 }
---
+++ src/Sections/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Assets/styles/hero.sass";
+
+function Hero() {
+    return (
+        <div id="hero">
+
+        </div>
+    )
+}
+
+export default Hero;
